97d4d8f7ebe8c7f5f5967a2e25295a32e08be37c
Sahar <saharco@campus.technion.ac.il>
2019-04-21 22:31:54 +0300

    implemented user authentication manager

--- courseapp-test/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/CourseAppStaffTest.kt
+++ courseapp-test/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/CourseAppStaffTest.kt
@@ -22,7 +22,6 @@ class CourseAppStaffTest {
     fun `after login, a user is logged in`() {
         courseApp.login("gal", "hunter2")
         courseApp.login("imaman", "31337")
-
         val token = courseApp.login("matan", "s3kr1t")
 
         assertThat(runWithTimeout(ofSeconds(10)) { courseApp.isUserLoggedIn(token, "gal") },
